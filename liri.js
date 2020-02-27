9513d06381634a6cc02becfbe0bc6d2d6e9b2883
Jennifer Chan <jenniferchan2267@yahoo.com>
2020-02-27 03:50:20 -0800

    add function for case concert-this

--- liri.js
+++ liri.js
@@ -8,20 +8,16 @@ var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
 var value = process.argv[3];
-var defaultMovie = "Mr. Nobody";
 
 switch (action) {
   case "concert-this":
-    getBands()
+    getBands(value)
     break;
   case "spotify-this-song":
-    getSongs()
+    getSongs(value)
     break;
   case "movie-this":
-    if (value == "") {
-      value = defaultMovie;
-    }
-    getMovies()
+    getMovies(value)
     break;
   case "do-what-it-says":
     doWhatItSays()
@@ -30,3 +26,16 @@ switch (action) {
     console.log("Choose one of the following commands: \nconcert-this <artist>, \nspotify-this-song <song name>, \nmovie-this <movie title>, \ndo-what-it-says")
     break;
 }
+
+function getBands(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+      .then(function (response) {
+        console.log("Name of the venue:", response.data[0].venue.name);
+        console.log("Venue location:", response.data[0].venue.city);
+        var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+        console.log("Date of the Event:", eventDate);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
